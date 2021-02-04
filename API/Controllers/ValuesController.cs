fd8d4553652a7369df79883d1a463025f3e93c5d
mwert09 <mwert09@gmail.com>
2021-02-04 18:53:02 +0300

    CRUD Application

--- API/Controllers/ValuesController.cs
+++ API/Controllers/ValuesController.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Domain;
+﻿using Domain;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Components;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace API.Controllers
 {
